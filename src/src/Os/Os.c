4e6863b36b4a5cb7614a213cd463bd2d258703ad
siredmar <knallerbse@gmail.com>
2015-10-17 15:28:25 +0200

    Os.c modified

--- src/src/Os/Os.c
+++ src/src/Os/Os.c
@@ -1,15 +1,15 @@
 /**
-* COPYRIGHT : GNU GPL V2 -
-*
-* This is the main source of the Os
-*
-* Author: Armin Schlegel
-* based on project eRTK (https://github.com/eRTK/eRTK)
-* Creation date: 2015-09-21
-* Last change: 2015-09-21
-*
-* Moduleversion: 0.0.1
-****************************************************************************** */
+ * COPYRIGHT : GNU GPL V2 -
+ *
+ * This is the main source of the Os
+ *
+ * Author: Armin Schlegel
+ * based on project eRTK (https://github.com/eRTK/eRTK)
+ * Creation date: 2015-09-21
+ * Last change: 2015-09-21
+ *
+ * Moduleversion: 0.0.1
+ ****************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
@@ -26,18 +26,18 @@
 static void Os_TimerInit(void);
 
 #ifdef OS_DEBUG
-static uint8 Os_Stack[OS_MAX_NUMBER_OF_TASKS+1][OS_STACKSIZE]  __attribute__ ((aligned (256)));
+static uint8 Os_Stack[OS_MAX_NUMBER_OF_TASKS+1][OS_STACKSIZE] __attribute__ ((aligned (256)));
 #else
-static uint8 Os_Stack[OS_MAX_NUMBER_OF_TASKS+1][OS_STACKSIZE];
+static uint8 Os_Stack[OS_MAX_NUMBER_OF_TASKS + 1][OS_STACKSIZE];
 #endif
 
 static Os_TaskConfigType *Os_TaskConfig;
 
-static void * Os_StackPointer[OS_MAX_NUMBER_OF_TASKS+1];
+static void * Os_StackPointer[OS_MAX_NUMBER_OF_TASKS + 1];
 
 static volatile Os_TaskIdType Os_CurrentRunningTask;
 
-Os_TaskControlBlockType Os_TaskControlBlock[OS_MAX_NUMBER_OF_TASKS+1];
+Os_TaskControlBlockType Os_TaskControlBlock[OS_MAX_NUMBER_OF_TASKS + 1];
 
 static Os_SemaphoreType Os_Semaphore[OS_MAX_NUM_OF_SEMAPHORE];
 
@@ -47,10 +47,10 @@ Os_TaskControlBlockType * TaskReadyList;
 volatile uint16 Os_CurrentPerformanceCount;        //aktueller counter
 volatile uint16 Os_PerformanceCounter[256]; //counter array der letzen 256ms
 volatile uint32 Os_Ticks = 0u;            //wie spaet ist es nach dem urknall in ms
-volatile uint8  Os_OverloadCounter;     //zaehlt die aufeinanderfolgenden overload phasen
+volatile uint8 Os_OverloadCounter;     //zaehlt die aufeinanderfolgenden overload phasen
 
-volatile uint8  Os_SystemStarted;                 //wird gesetzt wenn das system gestartet ist
-volatile uint8  Os_PerformanceCounterIndex;              //index im array 0..255
+volatile uint8 Os_SystemStarted;                 //wird gesetzt wenn das system gestartet ist
+volatile uint8 Os_PerformanceCounterIndex;              //index im array 0..255
 //#endif
 
 void Os_Init(void)
@@ -63,35 +63,35 @@ void Os_Init(void)
 
     Os_TaskConfig = (Os_TaskConfigType*) Os_GetLcfgData();
 
-    for(taskId = 0; taskId < OS_MAX_NUMBER_OF_TASKS + 1; taskId++)
+    for (taskId = 0; taskId < OS_MAX_NUMBER_OF_TASKS + 1; taskId++)
     {
         Os_TaskControlBlock[taskId].NextTask = NULL; /* verkettung der Os_TaskControlBlock's in unsortiertem grundzustand */
         Os_TaskControlBlock[taskId].PreviousTask = NULL;
         Os_TaskControlBlock[taskId].TaskId = taskId;
-        Os_TaskControlBlock[taskId].TaskPrio = taskId ? Os_TaskConfig[taskId-1].TaskPrio : 0; //idle task ist immer da mit pri 0
+        Os_TaskControlBlock[taskId].TaskPrio = taskId ? Os_TaskConfig[taskId - 1].TaskPrio : 0; //idle task ist immer da mit pri 0
         Os_TaskControlBlock[taskId].TaskTimer = 0;
         //die parameter merken zum debuggen
 #ifdef OS_DEBUG
-        Os_TaskControlBlock[taskId].param0 =  taskId ? 0 : Os_TaskConfig[taskId-1].param0;
-        Os_TaskControlBlock[taskId].param1 =  taskId ? 0 : Os_TaskConfig[taskId-1].param1;
+        Os_TaskControlBlock[taskId].param0 = taskId ? 0 : Os_TaskConfig[taskId-1].param0;
+        Os_TaskControlBlock[taskId].param1 = taskId ? 0 : Os_TaskConfig[taskId-1].param1;
 #endif
     }
 
     //einsetzen der idle task, die muss immer da sein !
-    TaskReadyList = (Os_TaskControlBlockType *)&Os_TaskControlBlock[0];
+    TaskReadyList = (Os_TaskControlBlockType *) &Os_TaskControlBlock[0];
     TaskReadyList->NextTask = NULL;
     TaskReadyList->PreviousTask = NULL;
 
     /* einsortieren der tasks mit verkettung nach absteigender prioritaet */
     char hit[OS_MAX_NUMBER_OF_TASKS + 1];
     memset(hit, 0, sizeof hit);
-    for(task = 1; task < OS_MAX_NUMBER_OF_TASKS + 1; task++)
+    for (task = 1; task < OS_MAX_NUMBER_OF_TASKS + 1; task++)
     {
         prio = 0;
         index = 0;
-        for(taskId = 1; taskId < OS_MAX_NUMBER_OF_TASKS + 1; taskId++)
+        for (taskId = 1; taskId < OS_MAX_NUMBER_OF_TASKS + 1; taskId++)
         { //hoechstpriore ready und noch nicht verkettete task finden
-            if(Os_TaskControlBlock[taskId].TaskPrio >= prio && !hit[taskId])
+            if (Os_TaskControlBlock[taskId].TaskPrio >= prio && !hit[taskId])
             { /* prio>prio und noch nicht in liste */
                 prio = Os_TaskControlBlock[taskId].TaskPrio;
                 index = taskId;
@@ -101,63 +101,59 @@ void Os_Init(void)
         hit[index] = 1;
     }
 
-    for(taskId = 0; taskId < OS_MAX_NUMBER_OF_TASKS + 1; taskId++)
+    for (taskId = 0; taskId < OS_MAX_NUMBER_OF_TASKS + 1; taskId++)
     {
-        for(uint16 f = 0; f < OS_STACKSIZE / 4; f++)
+        for (uint16 f = 0; f < OS_STACKSIZE / 4; f++)
         {
             memcpy(Os_Stack[taskId] + 4 * f, "\xde\xad\xbe\xef", 4);
         }
 
         /* startadressen und parameter auf den stack */
-        Os_Stack[taskId][OS_STACKSIZE-9] = 0; //r1 = 0
+        Os_Stack[taskId][OS_STACKSIZE - 9] = 0; //r1 = 0
 
         memset(&TaskAddress, 0, sizeof TaskAddress);
         TaskAddress.ErrorHookPointer = Os_ErrorHook;
-        Os_Stack[taskId][OS_STACKSIZE-1] = TaskAddress.ui8[0];
-        Os_Stack[taskId][OS_STACKSIZE-2] = TaskAddress.ui8[1];
-        Os_Stack[taskId][OS_STACKSIZE-3] = TaskAddress.ui8[2];
-        if(taskId)
+        Os_Stack[taskId][OS_STACKSIZE - 1] = TaskAddress.ui8[0];
+        Os_Stack[taskId][OS_STACKSIZE - 2] = TaskAddress.ui8[1];
+        Os_Stack[taskId][OS_STACKSIZE - 3] = TaskAddress.ui8[2];
+        if (taskId)
         {
-            TaskAddress.TaskPointer = Os_TaskConfig[taskId-1].TaskHandler;
+            TaskAddress.TaskPointer = Os_TaskConfig[taskId - 1].TaskHandler;
         }
         else
         {
             TaskAddress.IdleTaskPointer = Os_Idle;
         }
-        Os_Stack[taskId][OS_STACKSIZE-4] = TaskAddress.ui8[0];
-        Os_Stack[taskId][OS_STACKSIZE-5] = TaskAddress.ui8[1];
-        Os_Stack[taskId][OS_STACKSIZE-6] = TaskAddress.ui8[2];
+        Os_Stack[taskId][OS_STACKSIZE - 4] = TaskAddress.ui8[0];
+        Os_Stack[taskId][OS_STACKSIZE - 5] = TaskAddress.ui8[1];
+        Os_Stack[taskId][OS_STACKSIZE - 6] = TaskAddress.ui8[2];
 
-        if(taskId)
+        if (taskId)
         {
-            Os_Stack[taskId][OS_STACKSIZE-32] = Os_TaskConfig[taskId-1].param0 & 0xff;
-            Os_Stack[taskId][OS_STACKSIZE-33] = Os_TaskConfig[taskId-1].param0 >> 8;
-            Os_Stack[taskId][OS_STACKSIZE-30] = (uint16)(Os_TaskConfig[taskId-1].param1) & 0xff;
-            Os_Stack[taskId][OS_STACKSIZE-31] = (uint16)(Os_TaskConfig[taskId-1].param1) >> 8;
+            Os_Stack[taskId][OS_STACKSIZE - 32] = Os_TaskConfig[taskId - 1].param0 & 0xff;
+            Os_Stack[taskId][OS_STACKSIZE - 33] = Os_TaskConfig[taskId - 1].param0 >> 8;
+            Os_Stack[taskId][OS_STACKSIZE - 30] = (uint16) (Os_TaskConfig[taskId - 1].param1) & 0xff;
+            Os_Stack[taskId][OS_STACKSIZE - 31] = (uint16) (Os_TaskConfig[taskId - 1].param1) >> 8;
         }
 
-        Os_StackPointer[taskId] = &Os_Stack[taskId][OS_STACKSIZE-40];
+        Os_StackPointer[taskId] = &Os_Stack[taskId][OS_STACKSIZE - 40];
     }
     Os_SemaphoreInit();
 }
 
-
-
-
-
-void  __attribute__ ((optimize("O2"))) Os_Idle(void)
+void __attribute__ ((optimize("O2"))) Os_Idle(void)
 {
 #ifdef OS_DEBUG
     while(1)
-{           //14+2 = 16 cycles pro loop -> 16MHz -> 1000 inc/ms
-        cli();               //cli = 1clock
-        ++Os_CurrentPerformanceCount;    //lds,lds,adiw,sts,sts = 5x2clocks
-        sei();               //sei = 1clock
-        oIDLEfast(1);      //2 cycles fuer 2xnop oder ein output bit setzen
+    {           //14+2 = 16 cycles pro loop -> 16MHz -> 1000 inc/ms
+        cli();//cli = 1clock
+        ++Os_CurrentPerformanceCount;//lds,lds,adiw,sts,sts = 5x2clocks
+        sei();//sei = 1clock
+        oIDLEfast(1);//2 cycles fuer 2xnop oder ein output bit setzen
     }                     //rjmp = 2clocks
 #else
-    while(1)
-{
+    while (1)
+    {
         set_sleep_mode(SLEEP_MODE_IDLE);
         sleep_enable();
         sei();
@@ -171,9 +167,9 @@ void  __attribute__ ((optimize("O2"))) Os_Idle(void)
 __attribute__ ((noinline)) void Os_ErrorHook(Os_ErrorHookType error)
 {
     oIDLE(0);
-    while(1); //this is the end...
+    while (1)
+        ; //this is the end...
 }
-
 
 void * Os_PushPopStackPointer; //speicher fuer stackpointer waehrend push/pop
 
@@ -220,21 +216,22 @@ void __attribute__ ((naked)) Os_Scheduler(void)
 #endif
     Os_StackPointer[Os_CurrentRunningTask] = Os_PushPopStackPointer;
 
-    if(TaskReadyList)
+    if (TaskReadyList)
     {
         //do round robin bei mehreren mit gleicher prio
         register Os_TaskControlBlockType *p = TaskReadyList;
-        while(p->TaskId != Os_CurrentRunningTask)
+        while (p->TaskId != Os_CurrentRunningTask)
         {
             p = p->NextTask; //finde aktuelle task
-            if(!p) break;
+            if (!p)
+                break;
         }
-        if(p)
+        if (p)
         { //task stand noch in der ready liste
-            //teste pri des nachfolgers
-            if(p->NextTask != NULL)
+          //teste pri des nachfolgers
+            if (p->NextTask != NULL)
             { //wenn es nachfolger gibt
-                if(p->TaskPrio == p->NextTask->TaskPrio)
+                if (p->TaskPrio == p->NextTask->TaskPrio)
                 { //schalte weiter wenn nachfolger prio genauso ist
                     Os_CurrentRunningTask = p->NextTask->TaskId;
                 }
@@ -264,11 +261,10 @@ void __attribute__ ((naked)) Os_Scheduler(void)
     asm volatile ("ret");
 }
 
-
 void Os_Start(void)
 {
     /* Start with highest priority task */
-    if(TaskReadyList)
+    if (TaskReadyList)
     {
         Os_CurrentRunningTask = TaskReadyList->TaskId;
     }
@@ -280,7 +276,8 @@ void Os_Start(void)
     Os_SystemStarted = 1;
     Os_PushPopStackPointer = Os_StackPointer[Os_CurrentRunningTask];
 
-    Os_PopStack();
+    Os_PopStack()
+    ;
     asm volatile("ret");
 
     /* This code should never been reached if control structures are alright */
@@ -324,15 +321,17 @@ void Os_Start(void)
  */
 static __inline__ void Os_InsertTask(Os_TaskControlBlockType *pthis, Os_TaskControlBlockType *newone)
 {
-    if(!pthis || !newone) Os_ErrorHook(OS_SYS_NULLPTR);
+    if (!pthis || !newone)
+        Os_ErrorHook(OS_SYS_NULLPTR);
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
     {
-        if(pthis->PreviousTask == NULL)
+        if (pthis->PreviousTask == NULL)
         { //der erste Os_TaskControlBlock
             TaskReadyList = newone;
             newone->PreviousTask = NULL;
         }
-        else { //Os_TaskControlBlock in der kette
+        else
+        { //Os_TaskControlBlock in der kette
             pthis->PreviousTask->NextTask = newone;
             newone->PreviousTask = pthis->PreviousTask;
         }
@@ -345,20 +344,21 @@ static __inline__ void Os_RemoveTask(Os_TaskControlBlockType *pthis)
 {
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
     {
-        if(pthis == NULL || pthis == &Os_TaskControlBlock[0])
+        if (pthis == NULL || pthis == &Os_TaskControlBlock[0])
         { //0 oder idle task
             Os_ErrorHook(OS_SYS_NULLPTR);
         }
-        if(pthis->PreviousTask == NULL)
+        if (pthis->PreviousTask == NULL)
         { //es ist der erste block auf den pTskReady zeigt
-            if(pthis->NextTask == NULL)
+            if (pthis->NextTask == NULL)
             { //und er hat keinen nachfolger den man vorziehen kann
                 Os_ErrorHook(OS_SYS_NULLPTR);
             }
             TaskReadyList = pthis->NextTask; //nachfolger einhaengen
             pthis->NextTask->PreviousTask = NULL;
         }
-        else { //mittendrin in der kette
+        else
+        { //mittendrin in der kette
             pthis->PreviousTask->NextTask = pthis->NextTask;
             pthis->NextTask->PreviousTask = pthis->PreviousTask;
         }
@@ -387,45 +387,48 @@ void Os_SetReady(Os_TaskIdType TaskId)
     {
         Os_TaskControlBlock[TaskId].TaskTimer = 0; //timer loeschen damit er nicht spaeter nochmal startet
 
-        do { //pthis->hoechstprioren Os_TaskControlBlock oder idle
+        do
+        { //pthis->hoechstprioren Os_TaskControlBlock oder idle
             if(Os_TaskControlBlock[TaskId].TaskPrio > pthis->TaskPrio || pthis->NextTask == NULL)
             { //neuer eintrag hat hoehere prio oder es gibt keinen nachfolger
                 Os_InsertTask(pthis, &Os_TaskControlBlock[TaskId]);
                 break;
             }
-        } while((pthis = pthis->NextTask));
+        }while((pthis = pthis->NextTask));
     }
 }
 
 //pTskRdy->Os_TaskControlBlockx->Os_TaskControlBlocky->0 es muss immer mindestens ein Os_TaskControlBlock (idle) in der liste bleiben
 void Os_SetSuspended(Os_TaskIdType TaskId)
 { //Os_TaskControlBlock[TaskId] aus der ready list austragen
-    if(!TaskId)
+    if (!TaskId)
     {
         Os_ErrorHook(OS_SYS_NULLPTR); //idle task darf nicht suspendiert werden
     }
 
-    if(!Os_TaskControlBlock[TaskId].PreviousTask && !Os_TaskControlBlock[TaskId].NextTask) Os_ErrorHook(OS_SYS_VERIFY); //war nicht in ready list
+    if (!Os_TaskControlBlock[TaskId].PreviousTask && !Os_TaskControlBlock[TaskId].NextTask)
+        Os_ErrorHook(OS_SYS_VERIFY); //war nicht in ready list
     Os_TaskControlBlockType *pthis = TaskReadyList;
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
     {
-        do {
-            if(pthis->TaskId == TaskId)
+        do
+        {
+            if (pthis->TaskId == TaskId)
             {
                 Os_RemoveTask(pthis);
                 break;
             }
-        } while((pthis = pthis->NextTask));
+        } while ((pthis = pthis->NextTask));
     }
 }
 
 void Os_SuspendAndWait(uint8 timeout)
 {
-    if(timeout)
+    if (timeout)
     { //sonst klinkt sich die task in einem wait_until() fuer immer aus
         ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
         { //14+2 = 16 cycles pro loop -> 16MHz -> 1000 inc/ms
-            if(Os_TaskControlBlock[Os_CurrentRunningTask].TaskTimer)
+            if (Os_TaskControlBlock[Os_CurrentRunningTask].TaskTimer)
             {
                 Os_ErrorHook(OS_SYS_UNKNOWN);
             }
@@ -443,12 +446,11 @@ void Os_SuspendAndWait(uint8 timeout)
 #endif
 }
 
-
 void Os_SleepMs(uint16 ms)
 { //macht nur auf avr sinn
-    while(ms)
+    while (ms)
     {
-        if(ms > 255)
+        if (ms > 255)
         {
             Os_SuspendAndWait(255);
             ms -= 255;
@@ -476,11 +478,11 @@ static inline __attribute__ ((__always_inline__)) uint8 Os_ExchangeContent(Os_Se
 
 void Os_GetSema(Os_SemaphoreType semaid)
 { /* Warten bis Semaphore frei ist und danach besetzen */
-    if(semaid > OS_MAX_NUM_OF_SEMAPHORE)
+    if (semaid > OS_MAX_NUM_OF_SEMAPHORE)
     {
         Os_ErrorHook(OS_SYS_UNKNOWN);
     }
-    while(Os_ExchangeContent(&Os_Semaphore[semaid], 1))
+    while (Os_ExchangeContent(&Os_Semaphore[semaid], 1))
     { /* >0 = sema blockiert */
         sei();
         Os_SuspendAndWait(1);
@@ -489,7 +491,7 @@ void Os_GetSema(Os_SemaphoreType semaid)
 
 void Os_FreeSema(Os_SemaphoreType semaid)
 {
-    if(semaid > OS_MAX_NUM_OF_SEMAPHORE)
+    if (semaid > OS_MAX_NUM_OF_SEMAPHORE)
     {
         Os_ErrorHook(OS_SYS_UNKNOWN);
     }
@@ -499,7 +501,7 @@ void Os_FreeSema(Os_SemaphoreType semaid)
 void Os_SemaphoreInit(void)
 {
     static uint8 n;
-    for(n = 0; n < OS_MAX_NUM_OF_SEMAPHORE; n++)
+    for (n = 0; n < OS_MAX_NUM_OF_SEMAPHORE; n++)
     {
         Os_Semaphore[n] = 0; /* ff=keine task wartend */
     }
@@ -513,7 +515,7 @@ uint8 Os_GetTimer8(void)
 uint16 Os_GetTimer16(void)
 { //wenn 256ms nicht reichen
     register uint16 val;
-    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    ATOMIC_BLOCK (ATOMIC_RESTORESTATE)
     val = Os_TimerStruct.timer16[0];
     return val;
 }
@@ -529,19 +531,21 @@ void Os_WaitUntil(uint8 then)
 //schedule_immediately wenn true dann wird sofort eine neue Prozesstabelle ermittelt und der hoechstpriore prozess gestartet
 void Os_ChangeTaskPriority(Os_TaskIdType TaskId, Os_TaskPrioType prio, Os_ScheduleImmediatyleType schedule)
 {
-    if(TaskId == 0)
+    if (TaskId == 0)
     {
         TaskId = Os_CurrentRunningTask;
     }
 
-    if(TaskId < OS_MAX_NUMBER_OF_TASKS)
+    if (TaskId < OS_MAX_NUMBER_OF_TASKS)
     {
         Os_TaskControlBlock[Os_CurrentRunningTask].TaskPrio = prio;
-        if(schedule == OS_TASK_SCHEDULE_IMMEDIATELY)
+        if (schedule == OS_TASK_SCHEDULE_IMMEDIATELY)
         {
             Os_Scheduler();
         }
-    } else Os_ErrorHook(OS_SYS_VERIFY); //die task id ist unbekannt
+    }
+    else
+        Os_ErrorHook(OS_SYS_VERIFY); //die task id ist unbekannt
 }
 
 //damit im irq alle register gesichert werden
@@ -558,17 +562,17 @@ __inline__ void __attribute__ ((always_inline)) Os_Timertick(void)
     Os_Ticks++;
 //    }
 
-    ticktimer  =  (0xFFFF0000 & (Os_TimerStruct.timer16[1] << 8)) && (Os_TimerStruct.timer16[0] & 0xFFFF);
+    ticktimer = (0xFFFF0000 & (Os_TimerStruct.timer16[1] << 8)) && (Os_TimerStruct.timer16[0] & 0xFFFF);
     ticktimer++;
     Os_TimerStruct.timer16[0] = ticktimer & 0xFFFF;
     Os_TimerStruct.timer16[1] = (ticktimer >> 8) & 0xFFFF;
     register Os_TaskControlBlockType *p = Os_TaskControlBlock;
     //timer service
-    for(register uint8 n = 0; n < OS_MAX_NUMBER_OF_TASKS + 1; n++)
+    for (register uint8 n = 0; n < OS_MAX_NUMBER_OF_TASKS + 1; n++)
     {
-        if(p->TaskTimer)
+        if (p->TaskTimer)
         {
-            if(!--p->TaskTimer)
+            if (!--p->TaskTimer)
             {
                 Os_SetReady(p->TaskId);
             }
@@ -591,31 +595,27 @@ __inline__ void __attribute__ ((always_inline)) Os_Timertick(void)
         Os_CurrentPerformanceCount = 0;
     }
 #endif
-    if(Os_SystemStarted)
+    if (Os_SystemStarted)
     {
         Os_Scheduler();
     }
 }
 
-
-ISR(TIMER0_OVF_vect)
+ISR( TIMER0_OVF_vect)
 { //1kHz timer
-    TCNT0 = (uint8)(-1*OS_TIMER_PRELOAD);
+    TCNT0 = (uint8) (-1 * OS_TIMER_PRELOAD);
     Os_Timertick();
 }
 
 static void Os_TimerInit(void)
 {
-    TCCR0B = /*(1 << CS12)|*/(1 << CS01)|(1 << CS00); //prescaler CLK/64
+    TCCR0B = /*(1 << CS12)|*/(1 << CS01) | (1 << CS00); //prescaler CLK/64
     TIMSK0 |= (1 << TOIE0);
-    TCNT0 = (uint8)(-1*OS_TIMER_PRELOAD);
+    TCNT0 = (uint8) (-1 * OS_TIMER_PRELOAD);
 }
 
 void Os_TimerInit(void)
 {
     Os_TimerInit();
 }
-
-
-
 
